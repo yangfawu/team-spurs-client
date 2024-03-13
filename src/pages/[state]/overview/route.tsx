1c73c9868b5e8b79c5749bbb1cfaebfb9378e312
Yangfa Wu <60656310+ywu3760@users.noreply.github.com>
2024-03-13 22:55:55 +0000

    added a basic layout to toggle between modes

--- src/pages/[state]/overview/route.tsx
+++ src/pages/[state]/overview/route.tsx
@@ -1,8 +1,13 @@
 import { lazy } from "react"
 import { Route } from "react-router-dom"
 
+const Layout = lazy(() => import("./layout"))
 const Page = lazy(() => import("./page"))
 
-const OVERVIEW_ROUTER = <Route path="overview" Component={Page} />
+const OVERVIEW_ROUTER = (
+    <Route path="overview" Component={Layout}>
+        <Route index Component={Page} />
+    </Route>
+)
 
 export default OVERVIEW_ROUTER
