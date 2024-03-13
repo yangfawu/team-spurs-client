8c3afe79f2e6269719e170154b971dc2f7f1d927
Yangfa Wu <60656310+ywu3760@users.noreply.github.com>
2024-03-13 17:22:53 +0000

    removed unnecessary files from template

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,20 +1,11 @@
-import Footer from "@/components/footer"
-import Header from "@/components/header"
 import { Suspense } from "react"
 import { Outlet } from "react-router-dom"
 import Loading from "./loading"
 
 export default function Layout() {
     return (
-        <>
-            <Header />
-            <main className="flex-1">
-                <span className="sr-only">root layout</span>
-                <Suspense fallback={<Loading />}>
-                    <Outlet />
-                </Suspense>
-            </main>
-            <Footer />
-        </>
+        <Suspense fallback={<Loading />}>
+            <Outlet />
+        </Suspense>
     )
 }
--- src/pages/product/[pid]/route.tsx
+++
@@ -1,9 +0,0 @@
-import { lazy } from "react"
-import { Route } from "react-router-dom"
-
-const Page = lazy(() => import("./page"))
-
-// this route has no sub-routes, so no children
-const SLUG_ROUTER = <Route path=":pid" element={<Page />} />
-
-export default SLUG_ROUTER
