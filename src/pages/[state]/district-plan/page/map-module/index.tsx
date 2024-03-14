18a9ff02d276f4136a25f2add46710a160cbabac
Yangfa Wu <60656310+ywu3760@users.noreply.github.com>
2024-03-14 05:46:17 +0000

    updated refocus button to work based on layer data instead of preconfigured coordinates

--- src/pages/[state]/district-plan/page/map-module/index.tsx
+++ src/pages/[state]/district-plan/page/map-module/index.tsx
@@ -1,20 +1,30 @@
 import GrayWorldTileLayer from "@/components/leaflet/gray-world-tile-layer"
-import { Map } from "leaflet"
+import { LEAFLET_COORDINATES } from "@/constants/states"
+import useSelectedState from "@/hooks/use-selected-state"
+import { LatLngExpression, Map } from "leaflet"
 import "leaflet/dist/leaflet.css"
-import { Ref } from "react"
+import { Ref, useMemo } from "react"
 import { MapContainer } from "react-leaflet"
 import GeoLayer from "./geo-layer"
-import Refocus from "./refocus"
+import RefocusButton from "./refocus-button"
 
 interface Props {
     mapRef?: Ref<Map>
 }
 export default function MapModule({ mapRef }: Props) {
+    const [state_code] = useSelectedState()
+
+    const [center, zoom] = useMemo(() => {
+        const res = LEAFLET_COORDINATES.get(state_code)
+        if (res) return [res.center, res.zoom] as const
+        return [[0, 0], 6] as [LatLngExpression, number]
+    }, [state_code, LEAFLET_COORDINATES])
+
     return (
         <MapContainer
             className="w-full h-full"
-            center={[0, 0]}
-            zoom={6}
+            center={center}
+            zoom={zoom}
             minZoom={6}
             maxZoom={13}
             attributionControl={false}
@@ -23,7 +33,7 @@ export default function MapModule({ mapRef }: Props) {
         >
             <GrayWorldTileLayer />
             <GeoLayer />
-            <Refocus />
+            <RefocusButton />
         </MapContainer>
     )
 }
