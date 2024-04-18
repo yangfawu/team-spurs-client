e6b5f0d631a859affd9b95dc352468ec315d5706
Yangfa Wu <68346973+yangfawu@users.noreply.github.com>
2024-04-18 16:20:07 +0000

    added queryOption replacements for all RTK api functions

---
+++ src/api/representative.ts
@@ -0,0 +1,27 @@
+import Group from "@/constants/group"
+import Party from "@/constants/party"
+import State from "@/constants/state"
+import { queryOptions } from "@tanstack/react-query"
+
+export interface Representative {
+    first_name: string
+    last_name: string
+    district: number
+    party: Party
+    race: Group[]
+    image: string
+    state: State
+}
+
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/api/reps`
+
+export function fetchRepresentatives(state: State) {
+    return queryOptions<Representative[]>({
+        queryKey: ["representative", state],
+        queryFn: async () => {
+            const res = await fetch(`${BASE_URL}/${state}`)
+            const data: Representative[] = await res.json()
+            return data
+        },
+    })
+}
