465c6bcd0966d2717f81971ac9f3230324e596e2
Yangfa Wu <68346973+yangfawu@users.noreply.github.com>
2024-04-19 01:33:48 +0000

    added a reset button

--- src/components/control/index.tsx
+++ src/components/control/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@tanstack/react-router"
 import Logo from "../logo"
 import GroupSelection from "./group-selection"
 import ModeSelection from "./mode-selection"
@@ -10,6 +11,9 @@ export default function Control() {
             <StateSelection />
             <ModeSelection />
             <GroupSelection />
+            <div className="flex-1 flex items-center justify-end p-2">
+                <Link className="px-2 py-1 bg-gray-200 hover:bg-gray-400 rounded-full text-sm" to="/">Reset</Link>
+            </div>
         </div>
     )
 }
