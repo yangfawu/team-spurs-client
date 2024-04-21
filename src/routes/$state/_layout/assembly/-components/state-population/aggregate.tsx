e2df819cb6e5c8a0c6bc21f894ec62c3b78f30fa
yangfawu <yangfa.pop@gmail.com>
2024-04-21 14:50:07 +0000

    added an aggregate row to state population view

---
+++ src/routes/$state/_layout/assembly/-components/state-population/aggregate.tsx
@@ -0,0 +1,27 @@
+import { fetchStateDemographic } from "@/api/demographic"
+import { SUPPORTED_GROUPS } from "@/constants/group"
+import { useSafeCurrentState } from "@/contexts/current-state"
+import { useSuspenseQuery } from "@tanstack/react-query"
+import { useMemo } from "react"
+
+export default function Aggregate() {
+    const state = useSafeCurrentState()
+    const { data } = useSuspenseQuery(fetchStateDemographic(state))
+
+    const total = useMemo(() => {
+        let sum = 0
+        for (const group of SUPPORTED_GROUPS) {
+            if (group in data.count) {
+                sum += data.count[group]
+            }
+        }
+
+        return sum
+    }, [data])
+
+    return (
+        <p className="text-right text-sm italic">
+            Total: <b>{total.toPrecision(3)}</b>
+        </p>
+    )
+}
