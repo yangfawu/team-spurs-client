82ae4ac906c9b68507d15cfe9a7aa4f32f675481
yangfawu <yangfa.pop@gmail.com>
2024-04-21 15:28:31 +0000

    added redistricting module to assembly mode

--- src/routes/$state/_layout/assembly/-components/redistricting/index.tsx
+++ src/routes/$state/_layout/assembly/-components/redistricting/index.tsx
@@ -1,9 +1,22 @@
 import AssemblyView, { ASSEMBLY_VIEW_TO_NAME } from "@/constants/assembly-views"
+import { Suspense } from "react"
+import tw from "tailwind-styled-components"
+import Info from "./info"
 
 export default function Redistricting() {
     return (
-        <div className="h-full p-2">
-            <h3>{ASSEMBLY_VIEW_TO_NAME[AssemblyView.REDISTRICTING]}</h3>
-        </div>
+        <Container>
+            <h3 className="text-lg font-bold">{ASSEMBLY_VIEW_TO_NAME[AssemblyView.REDISTRICTING]}</h3>
+            <Suspense>
+                <Info />
+            </Suspense>
+        </Container>
     )
 }
+
+const Container = tw.div`
+    h-full
+    p-2
+    flex flex-col items-stretch gap-2
+    overflow-y-auto
+`
---
+++ src/routes/$state/_layout/assembly/-components/redistricting/info.tsx
@@ -0,0 +1,38 @@
+import { fetchRedistrictingInfo } from "@/api/assembly"
+import { useSafeCurrentState } from "@/contexts/current-state"
+import { LinkIcon } from "@heroicons/react/20/solid"
+import { useSuspenseQuery } from "@tanstack/react-query"
+import tw from "tailwind-styled-components"
+
+export default function Info() {
+    const state = useSafeCurrentState()
+    const {
+        data: { name, website, comments },
+    } = useSuspenseQuery(fetchRedistrictingInfo(state))
+
+    return (
+        <div className="space-y-1">
+            <h3 className="font-semibold">
+                <Link href={website}>
+                    <span>{name}</span>
+                    <LinkIcon className="w-3 h-3" />
+                </Link>
+            </h3>
+            <p className="text-sm p-2 bg-gray-100 border-l-4 border-l-gray-600">{comments}</p>
+        </div>
+    )
+}
+
+const Link = tw.a`
+    flex items-center gap-1
+    text-blue-500
+    hover:underline
+    hover:text-blue-700
+    active:text-blue-900
+    visited:text-purple-500
+    transition
+`
+Link.defaultProps = {
+    rel: "noopener noreferrer",
+    target: "_blank",
+}
