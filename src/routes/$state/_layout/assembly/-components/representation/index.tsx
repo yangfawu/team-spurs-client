ed6971c37932980502b965e9e59f2c33ef74b6b7
yangfawu <yangfa.pop@gmail.com>
2024-04-21 16:24:40 +0000

    added representation module to assembly mode

--- src/routes/$state/_layout/assembly/-components/representation/index.tsx
+++ src/routes/$state/_layout/assembly/-components/representation/index.tsx
@@ -1,9 +1,22 @@
+import TableLoader from "@/components/loader/table-loader"
 import AssemblyView, { ASSEMBLY_VIEW_TO_NAME } from "@/constants/assembly-views"
+import { Suspense } from "react"
+import tw from "tailwind-styled-components"
+import Chart from "./chart"
 
 export default function Representation() {
     return (
-        <div className="h-full p-2">
-            <h3>{ASSEMBLY_VIEW_TO_NAME[AssemblyView.REPRESENTATION]}</h3>
-        </div>
+        <Container>
+            <h3 className="text-lg font-bold">{ASSEMBLY_VIEW_TO_NAME[AssemblyView.REPRESENTATION]}</h3>
+            <Suspense fallback={<TableLoader />}>
+                <Chart />
+            </Suspense>
+        </Container>
     )
 }
+
+const Container = tw.div`
+    h-full
+    p-2
+    flex flex-col items-stretch gap-2
+`
